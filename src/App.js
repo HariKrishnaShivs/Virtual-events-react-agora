2c91f53fe24669fb2b26286c49682c350cc7f9d6
HariKrishna <68551077+HariKrishnaShivs@users.noreply.github.com>
2020-08-10 21:07:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import {BrowserRouter as Router,Switch,Route,Link }  from 'react-router-dom';
 function App() {
   return (
 
-  <Router>
+  <Router basename={window.location.pathname || ''}>
 
       <Switch>
 

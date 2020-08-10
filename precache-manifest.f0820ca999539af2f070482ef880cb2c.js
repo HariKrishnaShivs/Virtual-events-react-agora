099b7331dc28b9f4203572ee9394b9cc695aaa41
HariKrishnaShivs <sharikrishna562000@gmail.com>
2020-08-10 20:23:15 +0530

    Updates

---
+++ precache-manifest.f0820ca999539af2f070482ef880cb2c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22dfa2db29aec8411a2c7925b6ef4637",
+    "url": "/Virtual-events-react-agora/index.html"
+  },
+  {
+    "revision": "5b730cdb7c45ef4d5d48",
+    "url": "/Virtual-events-react-agora/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "044746be0c99471db20d",
+    "url": "/Virtual-events-react-agora/static/css/main.b8e9667b.chunk.css"
+  },
+  {
+    "revision": "5b730cdb7c45ef4d5d48",
+    "url": "/Virtual-events-react-agora/static/js/2.7d66bb14.chunk.js"
+  },
+  {
+    "revision": "05f7183a47d4367580280ef4913bf34b",
+    "url": "/Virtual-events-react-agora/static/js/2.7d66bb14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044746be0c99471db20d",
+    "url": "/Virtual-events-react-agora/static/js/main.776d7d9a.chunk.js"
+  },
+  {
+    "revision": "25d28d2bdb09eef274db",
+    "url": "/Virtual-events-react-agora/static/js/runtime-main.08f9ecd6.js"
+  },
+  {
+    "revision": "8f338a7d328486c6512d0da5db646b31",
+    "url": "/Virtual-events-react-agora/static/media/Indian-wedding.8f338a7d.jpeg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Virtual-events-react-agora/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "efacd419c66358f66f6a01ebad5a6219",
+    "url": "/Virtual-events-react-agora/static/media/messagelogo.efacd419.png"
+  },
+  {
+    "revision": "7bb02bef12a4d8c37f411c3991b7b5ee",
+    "url": "/Virtual-events-react-agora/static/media/ptcpslogo.7bb02bef.png"
+  },
+  {
+    "revision": "0a747d36187788f4a13e5804616a3cc6",
+    "url": "/Virtual-events-react-agora/static/media/vd-stream-img.0a747d36.webp"
+  }
+]);
